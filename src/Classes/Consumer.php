a8794120e0812c6deb967a4a57f6f56ca31cc349
Francisco Trompiz <trober131@gmail.com>
2022-01-24 11:00:13 +0100

    - Se agrego mas documetnacion en el proyecto

--- src/Classes/Consumer.php
+++ src/Classes/Consumer.php
@@ -26,7 +26,7 @@ class Consumer {
     public $queue_name = 'jobs';
 
     public $url = 'localhost';
-    public $port = '5672';
+    public $port = 5672;
 
     public $user = 'guest';
     public $password = 'guest';
@@ -34,33 +34,38 @@ class Consumer {
 
     public function __construct()
     {
+        // inicializamos la clase email sender para ser utilizada luego cuando se tenga que enviar algo
         $this->email_sender = new EmailSender();
     }
 
     public function connectToRabbitMQ()
     {
-        $this->connection = new AMQPStreamConnection($this->url, 5672, $this->user, $this->password);
+        //inicar la conection al servidor de rabbitmq
+        $this->connection = new AMQPStreamConnection($this->url, $this->port, $this->user, $this->password);
+        // nos conectamos al canal donde se enviara los mensajes
         $this->channel = $this->connection->channel();
     }
 
     public function initializeWaiting()
     {
-        $callback = function ($msg) {
+        $this->channel->basic_consume($this->queue_name, '', false, true, false, false, function ($msg) {
+            // cuando llegue el mensaje pintamos el resultado
             echo ' [x] Received ', "\n";
             print_r($msg->body);
             echo "\n";
 
+            //hacemos un json decoe para que podamos acceder a la informacion mas facilmente
             $msg_encoded = json_decode($msg->body,true);
 
             // Envio de mensaje via PHP
             $this->email_sender->sendEmailViaPHP($msg_encoded);
             // Envio de mensaje via MailChimp
             //$this->email_sender->sendEmailViaMailChimp($msg_encoded);
-        };
+        });
 
-        $this->channel->basic_consume($this->queue_name, '', false, true, false, false, $callback);
-
+        // iniciar loop infinito donde el consumer esperará los mensajes que este en la conal
         while ($this->channel->is_consuming()) {
+            // se imprimen los mensajes que  llegan de la cola
             echo ' [x] Waiting for messages ', "\n";
             $this->channel->wait();
         }
@@ -71,7 +76,9 @@ class Consumer {
      */
     public function closeConnection()
     {
+        // cerramos el canal
         $this->channel->close();
+        // cerramos la conneccion
         $this->connection->close();
     }
 }
--- src/Classes/Producer.php
+++ src/Classes/Producer.php
@@ -19,7 +19,7 @@ class Producer
     public $queue_name = 'jobs';
 
     public $url = 'localhost';
-    public $port = '5672';
+    public $port = 5672;
 
     public $user = 'guest';
     public $password = 'guest';
@@ -30,17 +30,23 @@ class Producer
 
     public function connectToRabbitMQ()
     {
-        $this->connection = new AMQPStreamConnection($this->url, 5672, $this->user, $this->password);
+        // inicializamos la conneccion al servidor de rabitmq
+        $this->connection = new AMQPStreamConnection($this->url, $this->port, $this->user, $this->password);
+        // nos conectamos al canal
         $this->channel = $this->connection->channel();
 
+        // creamos una cola, en caso que exista en el servidor de rabitmq, esta no se vuelve a crear
         $this->channel->queue_declare($this->queue_name, false, true, false, false);
     }
 
     public function sendMessage($msg_json)
     {
+        // creamos el mensaje que sera enviado a la cola
         $msg_to_send = new AMQPMessage(json_encode($msg_json));
+        // se envia a la cola el mensaje creado
         $this->channel->basic_publish($msg_to_send, '', $this->queue_name);
 
+        // imprimimos el mensaje enviado en el terminal
         echo " [x] Sent message \n";
         print_r($msg_to_send->body);
         echo "\n";
